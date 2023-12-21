68646c7243703237f179ead8d172840f96512141
Jan Jones <janjones@microsoft.com>
2023-12-21 15:18:32 +0100

    Add OAuth flow skeleton

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Nodes;
+using System.Web;
 using VsInsertions.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +22,23 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.MapGet("/oauth/callback", async (HttpContext context, string code, IConfiguration configuration) =>
+{
+    // Authorize app.
+    var config = configuration.GetSection("AzureDevOpsOAuth");
+    var client = new HttpClient();
+    var response = await client.PostAsync("https://app.vssps.visualstudio.com/oauth2/token", new FormUrlEncodedContent([
+        new("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"),
+        new("client_assertion", config.GetValue<string>("ClientSecret")),
+        new("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer"), 
+        new("assertion", code),
+        new("redirect_uri", $"{context.Request.Scheme}://{context.Request.Host}/oauth/callback")]));
+    var str = await response.Content.ReadAsStringAsync();
+    var json = JsonNode.Parse(str)!;
+
+    return Results.LocalRedirect("/?token=" + HttpUtility.UrlEncode(json["access_token"]!.ToString()));
+});
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
