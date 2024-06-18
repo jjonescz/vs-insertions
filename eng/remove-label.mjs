cfcbd9ef192f62846d0714965f08349233adfd4b
Jan Jones <janjones@microsoft.com>
2024-06-18 16:19:35 +0200

    Fix script logging

--- eng/remove-label.mjs
+++ eng/remove-label.mjs
@@ -10,7 +10,7 @@ export default async ({ context, github }) => {
 
     for (const pull of pulls.data) {
         if (pull.number !== context.issue.number) {
-            console.log(`Removing label from pull request #${pull.id}`);
+            console.log(`Removing label from pull request #${pull.number}`);
             await github.rest.issues.removeLabel({
                 owner: context.repo.owner,
                 repo: context.repo.repo,
