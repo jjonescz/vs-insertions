79fce8d3bfdc48c74254827785bf30634a4d9bb3
Jan Jones <janjones@microsoft.com>
2026-03-11 17:27:06 +0100

    Unify the table

---
+++ src/VsInsertions/UnifiedFlowRow.cs
@@ -0,0 +1,10 @@
+namespace VsInsertions;
+
+public sealed class UnifiedFlowRow
+{
+    public required string OtherRepo { get; init; }
+    public required bool IsIncoming { get; init; }
+    public ArcadeSubscription? Subscription { get; init; }
+    public string? SourceBranch { get; init; }
+    public List<FlowPr> Prs { get; init; } = [];
+}
